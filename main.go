370b6560434bc2ebe2b67a02e2c25c07a05a0a93
502659282 <f466ecc05f80b31c6960a36b94e69c36daa4ebc0>
2017-06-09 17:51:09 +0800

    Create

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "FriendsManagement/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "postgres", "postgres://postgres:1qaz2wsx3EDC@192.168.10.199:5432/FMDB?sslmode=disable")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
